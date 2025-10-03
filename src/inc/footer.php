4452bf5233abcbe039341d83e05d99dc124795aa
bafplus <bart.cockheyt@gmail.com>
2025-10-03 22:56:34 +0200

    Update footer.php

--- src/inc/footer.php
+++ src/inc/footer.php
@@ -9,10 +9,10 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="adminlte/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/adminlte/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="adminlte/dist/js/adminlte.min.js"></script>
+<script src="/adminlte/dist/js/adminlte.min.js"></script>
 </body>
 </html>
