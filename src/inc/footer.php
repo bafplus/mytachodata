a3ca494758da2862249d9e6ae98167794c46ee88
bafplus <bart.cockheyt@gmail.com>
2025-10-04 00:01:18 +0200

    Update footer.php

--- src/inc/footer.php
+++ src/inc/footer.php
@@ -5,14 +5,14 @@
   </div>
   <strong>&copy; 2025 MyTacho</strong> All rights reserved.
 </footer>
-</div>
-<!-- ./wrapper -->
+</div> <!-- /.wrapper -->
 
 <!-- jQuery -->
-<script src="adminlte/plugins/jquery/jquery.min.js"></script>
+<script src="/adminlte/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap -->
-<script src="adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="adminlte/dist/js/adminlte.min.js"></script>
+<script src="/adminlte/dist/js/adminlte.min.js"></script>
 </body>
 </html>
+
