fa5539c1008103d1fed8a37ae9714dee0c2c4133
bafplus <bart.cockheyt@gmail.com>
2025-10-03 17:00:18 +0200

    Create footer.php

---
+++ src/inc/footer.php
@@ -0,0 +1,8 @@
+  </div><!-- /.content-wrapper -->
+</div><!-- /.wrapper -->
+
+<script src="/adminlte/plugins/jquery/jquery.min.js"></script>
+<script src="/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/adminlte/dist/js/adminlte.min.js"></script>
+</body>
+</html>
