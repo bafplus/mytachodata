7fc9199a54410df2b0e40dc3d762b2559a840252
bafplus <bart.cockheyt@gmail.com>
2025-10-03 20:30:23 +0200

    Update sidebar.php

--- src/inc/sidebar.php
+++ src/inc/sidebar.php
@@ -1,18 +1,30 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
     <a href="index.php" class="brand-link">
-        <span class="brand-text font-weight-light">MyTacho</span>
+      <span class="brand-text font-weight-light">MyTacho</span>
     </a>
-    <div class="sidebar">
-        <nav>
-            <ul class="nav nav-pills nav-sidebar flex-column">
-                <li class="nav-item">
-                    <a href="index.php" class="nav-link active">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
-                        <p>Dashboard</p>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    </div>
-</aside>
 
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
+          <li class="nav-item">
+            <a href="index.php" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="upload-raw.php" class="nav-link">
+              <i class="nav-icon fas fa-upload"></i>
+              <p>Upload DDD</p>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
