2e12c2733a6afd4e0f1f89fdd7042ad16b8b90b4
bafplus <bart.cockheyt@gmail.com>
2025-10-03 23:46:16 +0200

    Update sidebar.php

--- src/inc/sidebar.php
+++ src/inc/sidebar.php
@@ -2,46 +2,21 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
   <a href="index.php" class="brand-link">
-    <i class="fas fa-truck nav-icon ml-2"></i>
     <span class="brand-text font-weight-light">MyTacho</span>
   </a>
 
   <!-- Sidebar -->
   <div class="sidebar">
-    <!-- Sidebar user panel (optional) -->
-    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-      <div class="image">
-        <img src="/adminlte/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-      </div>
-      <div class="info">
-        <a href="#" class="d-block"><?= htmlspecialchars($_SESSION['user'] ?? 'Guest') ?></a>
-      </div>
-    </div>
-
-    <!-- SidebarSearch Form -->
-    <div class="form-inline">
-      <div class="input-group" data-widget="sidebar-search">
-        <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-        <div class="input-group-append">
-          <button class="btn btn-sidebar">
-            <i class="fas fa-search fa-fw"></i>
-          </button>
-        </div>
-      </div>
-    </div>
-
     <!-- Sidebar Menu -->
     <nav class="mt-2">
-      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
-        <!-- Dashboard -->
+      <ul class="nav nav-pills nav-sidebar flex-column" 
+          data-widget="treeview" role="menu" data-accordion="false">
         <li class="nav-item">
           <a href="index.php" class="nav-link">
             <i class="nav-icon fas fa-tachometer-alt"></i>
             <p>Dashboard</p>
           </a>
         </li>
-
-        <!-- Upload -->
         <li class="nav-item">
           <a href="upload-raw.php" class="nav-link">
             <i class="nav-icon fas fa-upload"></i>
@@ -54,4 +29,3 @@
   </div>
   <!-- /.sidebar -->
 </aside>
-
