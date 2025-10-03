ab2fefdf25fb431349f05a4204e56bfe5f3c0a89
bafplus <bart.cockheyt@gmail.com>
2025-10-03 20:14:11 +0200

    Update header.php

--- src/inc/header.php
+++ src/inc/header.php
@@ -8,11 +8,3 @@
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
-    <!-- Navbar -->
-    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="logout.php">Logout</a>
-            </li>
-        </ul>
-    </nav>
