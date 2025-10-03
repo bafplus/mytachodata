9bf7f656f7fcc605914114bbd2b8e624fe443ab6
bafplus <bart.cockheyt@gmail.com>
2025-10-03 21:30:00 +0200

    Update header.php

--- src/inc/header.php
+++ src/inc/header.php
@@ -8,6 +8,8 @@
   <link rel="stylesheet" href="adminlte/plugins/fontawesome-free/css/all.min.css">
   <!-- AdminLTE CSS -->
   <link rel="stylesheet" href="adminlte/dist/css/adminlte.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
