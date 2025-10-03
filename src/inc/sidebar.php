d15ef4118f5f25060f2e0119efe7ff1f191e1f7d
bafplus <bart.cockheyt@gmail.com>
2025-10-04 00:30:47 +0200

    Update sidebar.php

--- src/inc/sidebar.php
+++ src/inc/sidebar.php
@@ -11,13 +11,13 @@
     <nav class="mt-2">
       <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
         <li class="nav-item">
-          <a href="index.php" class="nav-link">
+          <a href="index.php" class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'index.php' ? 'active' : ''; ?>">
             <i class="nav-icon fas fa-tachometer-alt"></i>
             <p>Dashboard</p>
           </a>
         </li>
         <li class="nav-item">
-          <a href="upload-raw.php" class="nav-link">
+          <a href="upload-raw.php" class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'upload-raw.php' ? 'active' : ''; ?>">
             <i class="nav-icon fas fa-upload"></i>
             <p>Upload DDD</p>
           </a>
