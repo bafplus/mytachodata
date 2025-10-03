c58c842a6a025d2d5be8c380a535daef59fadcb8
bafplus <bart.cockheyt@gmail.com>
2025-10-03 20:14:54 +0200

    Update sidebar.php

--- src/inc/sidebar.php
+++ src/inc/sidebar.php
@@ -1,13 +1,12 @@
-<!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="index.php" class="brand-link">
         <span class="brand-text font-weight-light">MyTacho</span>
     </a>
     <div class="sidebar">
-        <nav class="mt-2">
+        <nav>
             <ul class="nav nav-pills nav-sidebar flex-column">
                 <li class="nav-item">
-                    <a href="index.php" class="nav-link">
+                    <a href="index.php" class="nav-link active">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>Dashboard</p>
                     </a>
@@ -16,3 +15,4 @@
         </nav>
     </div>
 </aside>
+
