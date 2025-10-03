5793d8ec6a9b270cf8cc6dbccb52961737e9d9e7
bafplus <bart.cockheyt@gmail.com>
2025-10-03 23:46:39 +0200

    Update footer.php

--- src/inc/footer.php
+++ src/inc/footer.php
@@ -1,21 +1,18 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+  <div class="float-right d-none d-sm-block">
+    <b>Version</b> 1.0
   </div>
-  <!-- /.content-wrapper -->
-
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 1.0
-    </div>
-    <strong>&copy; 2025 MyTacho</strong> All rights reserved.
-  </footer>
+  <strong>&copy; 2025 MyTacho</strong> All rights reserved.
+</footer>
 </div>
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="/adminlte/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="adminlte/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="/adminlte/dist/js/adminlte.min.js"></script>
+<script src="adminlte/dist/js/adminlte.min.js"></script>
 </body>
 </html>
