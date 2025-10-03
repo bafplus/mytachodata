b0b3211bef634addfb53121733208693fcca16f9
bafplus <bart.cockheyt@gmail.com>
2025-10-03 23:26:17 +0200

    Update footer.php

--- src/inc/footer.php
+++ src/inc/footer.php
@@ -12,10 +12,10 @@
 <script src="/adminlte/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
 <script src="/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- overlayScrollbars -->
-<script src="/adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- AdminLTE App -->
 <script src="/adminlte/dist/js/adminlte.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="/adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 </body>
 </html>
 
