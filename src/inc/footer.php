bb7675a222a6f6357516c1275c939d64918c5fc2
bafplus <bart.cockheyt@gmail.com>
2025-10-03 18:45:12 +0200

    Update footer.php

--- src/inc/footer.php
+++ src/inc/footer.php
@@ -1,8 +1,15 @@
-  </div><!-- /.content-wrapper -->
-</div><!-- /.wrapper -->
+    <!-- Footer -->
+    <footer class="main-footer">
+        <div class="float-right d-none d-sm-inline">
+            MyTacho v1.0
+        </div>
+        <strong>&copy; 2025 MyTacho</strong>
+    </footer>
+</div> <!-- /.wrapper -->
 
-<script src="/adminlte/plugins/jquery/jquery.min.js"></script>
-<script src="/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="/adminlte/dist/js/adminlte.min.js"></script>
+<script src="adminlte/plugins/jquery/jquery.min.js"></script>
+<script src="adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="adminlte/dist/js/adminlte.min.js"></script>
 </body>
 </html>
+
