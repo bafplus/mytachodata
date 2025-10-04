013e4c577a4307769dc98ce788229982c9befd93
bafplus <bart.cockheyt@gmail.com>
2025-10-04 13:35:59 +0200

    Create en.php

---
+++ src/lang/en.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    "dashboard" => "Dashboard",
+    "upload" => "Upload DDD",
+    "logout" => "Logout",
+    "user_page" => "User Page",
+    "theme_light" => "Light Theme",
+    "theme_dark" => "Dark Theme",
+];
