806544f5a2687abbb4e4bd26d6a1cb10e766fd0b
bafplus <bart.cockheyt@gmail.com>
2025-10-04 13:35:36 +0200

    Delete src/en.php

--- src/en.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    "dashboard" => "Dashboard",
-    "upload" => "Upload DDD",
-    "logout" => "Logout",
-    "user_page" => "User Page",
-    "theme_light" => "Light Theme",
-    "theme_dark" => "Dark Theme",
-];
