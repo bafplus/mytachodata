8bec5b24bab64c832d77a2f68bda0f912cff7e12
bafplus <bart.cockheyt@gmail.com>
2025-10-04 15:21:45 +0200

    Create nl.php

---
+++ src/lang/nl.php
@@ -0,0 +1,14 @@
+<?php
+$lang_arr = [
+    // General
+    "dashboard" => "Dashboard",
+    "upload" => "Upload DDD",
+    "logout" => "Uitloggen",
+    "save" => "Opslaan",
+
+    // User Settings
+    "user_settings" => "Gebruikersinstellingen",
+    "profile" => "Profiel",
+    "username" => "Gebruikersnaam",
+    "language" => "Taal",
+];
