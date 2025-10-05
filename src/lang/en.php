385767e498c1bd8f881e46668c7cae9412b50676
bafplus <bart.cockheyt@gmail.com>
2025-10-05 12:24:11 +0200

    Update en.php

--- src/lang/en.php
+++ src/lang/en.php
@@ -1,5 +1,11 @@
 <?php
 return [
+    'dashboard_title' => 'MyTacho Dashboard',
+    'nav_home' => 'Home',
+    'nav_toggle_theme' => 'Toggle Light/Dark',
+    'nav_user' => 'User Page',
+    'nav_logout' => 'Logout',
+    
     'user_settings' => 'User Settings',
     'username' => 'Username',
     'new_password' => 'New Password',
