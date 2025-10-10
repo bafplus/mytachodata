c1b781b987dd785c22190798ccca8afe055cc022
bafplus <bart.cockheyt@gmail.com>
2025-10-10 17:56:42 +0200

    Update footer.php

--- src/inc/footer.php
+++ src/inc/footer.php
@@ -1,7 +1,7 @@
 <!-- Main Footer -->
 <footer class="main-footer">
   <div class="float-right d-none d-sm-block">
-    <b>Version</b> 1.0.9
+    <b>Version</b> 1.0.10
   </div>
   <strong>&copy; <?=(date('Y')>2013?' '.date('Y'):'')?> - MyTachoData.Com</strong> Your Card, Your Data!
 </footer>
